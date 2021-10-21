cc722495711d8fef9575052d524e2be014665c9e
Nadezhda <nad388@yandex.ru>
2021-10-21 21:19:49 +0300

    task3

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
-
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money, time;
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+start();
 
 let appData = {
     budget: money,
@@ -10,34 +17,66 @@ let appData = {
     },
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
-    
-    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
-        && a != '' && b != '' && a.length < 50) {
-        console.log('done');
-        appData.expenses[a] = b; // записываем ответ в объект. ключ-значение
-    } else {
-        console.log ("bad result");
-        i--;
-    }
-};
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Cредний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка");
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+        
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
+            && a != '' && b != '' && a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = b; // записываем ответ в объект. ключ-значение
+        } else {
+            console.log ("bad result");
+            i--;
+        }
+    };
 }
+chooseExpenses();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Cредний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+detectLevel();
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+        
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+checkSavings();
+
+function chooseOptExpenses() {
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?", "");
+        if (typeof (questionOptExpenses) === 'string' && typeof (questionOptExpenses) != null && questionOptExpenses != '' && questionOptExpenses.length < 50) {
+            console.log('done');
+            appData.optionalExpenses[i] = questionOptExpenses; // записываем ответ в объект. ключ-значение
+        } else {
+            console.log("bad result");
+            i--;
+        }
+    }
+}
+chooseOptExpenses();
